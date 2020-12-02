b4cbe0ccb4024d050bcd20dec2fd5bf7049a0f0b
Sh4d0w_小白 <50265741+914525753@users.noreply.github.com>
2020-12-02 21:05:07 +0800

    Add files via upload

--- php/XssBlindAdmin.php
+++ php/XssBlindAdmin.php
@@ -11,31 +11,28 @@
         Free($_POST['id'], $id);
     }
 
-    if (isset($_POST['query']) && $_POST['query'] === "1")
-    {
-        $sql = "select * from xssblind";
-        $result = mysqli_query($conn, $sql);
-        echo "<table><tr><th>id</th>";
-        echo "<th>time</th>";
-        echo "<th>content</th>";
-        echo "<th>name</th>";
-        echo "<th>operate</th></tr>"; 
-        if ($result->num_rows > 0) 
-        {          
-            while($row = $result->fetch_assoc())
-            {
-                echo "<tr><td>$row[id]</td>";
-                echo "<td>$row[time]</td>";
-                echo "<td>$row[content]</td>";
-                echo "<td>$row[name]</td>";
-                echo "<td><a onclick = Delete($row[id])>删除</a></td></tr>";
-                Free($row);
-            }
-        }
-        echo "</table>";
-        $result->free();
-        $conn->close();
-        Free($_POST['query'], $sql, $result, $conn);
-        die();
-    }
+	$sql = "select * from xssblind";
+	$result = mysqli_query($conn, $sql);
+	echo "<table><tr><th>id</th>";
+	echo "<th>time</th>";
+	echo "<th>content</th>";
+	echo "<th>name</th>";
+	echo "<th>operate</th></tr>"; 
+	if ($result->num_rows > 0) 
+	{          
+		while($row = $result->fetch_assoc())
+		{
+			echo "<tr><td>$row[id]</td>";
+			echo "<td>$row[time]</td>";
+			echo "<td>$row[content]</td>";
+			echo "<td>$row[name]</td>";
+			echo "<td><a onclick = Delete($row[id])>删除</a></td></tr>";
+			Free($row);
+		}
+	}
+	echo "</table>";
+	$result->free();
+	$conn->close();
+	Free($sql, $result, $conn);
+	die();
 ?>
