27ecaacc8474ef4e88e80f8dbd8861f4eb435eb7
Sh4d0w_小白 <50265741+914525753@users.noreply.github.com>
2021-02-18 19:59:31 +0800

    Add files via upload

---
+++ IdSql.php
@@ -0,0 +1,45 @@
+<?php
+    require_once "publicfunc.php";
+
+    if(isset($_POST["username"]) && isset($_POST["password"]) && !empty($_POST["username"]) && !empty($_POST["password"]))
+    {
+        $username = PurifyData($_POST["username"]);
+        $password = PurifyData($_POST["password"]);
+        if($username === "admin" && $password === "p@ssw0rd")
+        {
+            Free($username, $password);
+            FlagShow();
+        }
+        else echo "Username or Password error！";
+        Free($username, $password);
+        die();
+    }
+
+    if (!isset($_POST["username"]) && !isset($_POST["password"]))
+    {   
+		$id = (!isset($_GET["id"]))?1:$_GET["id"] ;
+		Free($_GET["id"]);
+        $sql = "select * from news where id = $id";
+        $result = mysqli_query($conn, $sql);
+        echo "<table><tr><th>id</th>";
+        echo "<th>username</th></tr>";
+		if($result === FALSE)
+		{ 
+			die();
+		}
+        if ($result->num_rows > 0) 
+        {
+			$row = null;
+            while($row = $result->fetch_assoc())
+            {
+                echo "<tr><td> $row[id]</td>";
+                echo "<td> $row[username]</td></tr>";
+            }
+        }
+        echo "</table>";
+        mysqli_free_result($result);
+        mysqli_close($conn);
+        Free($row, $id);
+        die();
+    }
+?>
---
+++ publicfunc.php
@@ -0,0 +1,58 @@
+<?php
+	$conn = mysqli_connect("127.0.0.1","root","","Secure_Development");
+	date_default_timezone_set("Asia/Shanghai"); //设置时间为中国时区
+	mysqli_set_charset($conn, 'utf-8'); //设置MYSQL为UTF-8
+	error_reporting(E_ALL^E_NOTICE^E_WARNING);
+
+	function PurifyData($data)
+	{
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data,ENT_QUOTES);	
+		$data = filter_var($data,FILTER_SANITIZE_STRING);
+		//$data = filter_var($data,FILTER_SANITIZE_MAGIC_QUOTES);
+		return strip_tags($data);
+	}
+
+	function GetFlag($url)
+	{
+		$flag = PurifyData($_POST["Flag"]);
+		$salt = "edbd92baa6a824f0d89a0a817d5dc5dd";
+		if ($flag == md5($url.$salt))
+		{
+			echo 1;
+		}
+		else
+		{
+			echo 0;
+		}
+		Free($flag,$salt,$url,$_POST["Flag"]);
+		die();
+	}
+
+	function FlagShow()
+	{
+		$flag = md5($_SERVER['PHP_SELF'].'edbd92baa6a824f0d89a0a817d5dc5dd');
+		echo "<br>flag：$flag";
+		Free($flag);
+		die();
+	}
+
+	if (isset($_POST["Flag"]))
+	{
+		GetFlag($_POST["url"]);
+		Free($_POST["url"]);
+	}
+
+	function Free(...$var)
+	{
+		foreach($var as $var)
+		{
+			if(isset($var))
+			{
+				$var = null;
+				unset($var);
+			}
+		}
+	}
+?>
