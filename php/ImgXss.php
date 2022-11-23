95af6e59ea019853ee87883a0615af25b3600535
shadowabi <50265741+shadowabi@users.noreply.github.com>
2022-11-23 21:47:55 +0800

    Create ImgXss.php

--- php/ImgXss.php
+++ php/ImgXss.php
@@ -4,7 +4,7 @@
 	if(isset($_GET["imgName"]) && !empty($_GET["imgName"]))
 	{
 		echo "<img src = 127.0.0.1/SecureDevelopment/".$_GET["imgName"]."></img>";
-		Free($_GET["imgName"]);
+		//Free($_GET["imgName"]);
 	}
 	die();
 ?>
